f0b902a21de428d5abea3330af89ed598b2fcab3
郑壮 <zhengzhuang@zhengzhngdeMBP2.www.idazoo.com>
2022-07-26 09:35:46 +0800

    :rocket: first commit

--- src/constants/env.js
+++ src/constants/env.js
@@ -2,16 +2,13 @@
  * @Author: {zhengzhuang}
  * @Date: 2022-05-26 17:31:36
  * @LastEditors: {zhengzhuang}
- * @LastEditTime: 2022-07-06 14:25:03
+ * @LastEditTime: 2022-07-26 09:35:24
  * @Description: In User Settings Edit
  */
-
 // 当前环境变量，development production
 /* eslint-disable */
 export const currentEnv = process.env.NODE_ENV;
 /* eslint-disable */
-
-// const env = currentEnv !== "production" ? `远度云${currentEnv}` : "远度云";
 
 const env =
   currentEnv === "development"
@@ -22,7 +19,7 @@ const env =
 
 // 不要删除，用来识别当前项目环境
 console.log(
-  `\n %c 远度云${env} %c http://www.yuanduyun.com \n`,
+  `\n %c 小郑${env} %c https://github.com/zhengzhuang96/ \n`,
   "color: #fff; background: #008bf8; padding:5px 0; font-size:12px;font-weight: bold;",
   "background: #008bf8; padding:5px 0; font-size:12px;"
 );
@@ -33,13 +30,13 @@ export const isPreEnv = currentEnv === "production";
 let baseUrlPrefix = "";
 switch (env) {
   case "development":
-    baseUrlPrefix = "https://test-zuhuoda-wx.yuanduyun.com";
+    baseUrlPrefix = "https://dev-*****.com";
     break;
   case "testEnv":
-    baseUrlPrefix = "https://test-zuhuoda-wx.yuanduyun.com";
+    baseUrlPrefix = "https://test-***.com";
     break;
   case "production":
-    baseUrlPrefix = "https://zuhuoda-wx.yuanduyun.com";
+    baseUrlPrefix = "https://***.com";
     break;
 }
 export const baseUrl = baseUrlPrefix;
